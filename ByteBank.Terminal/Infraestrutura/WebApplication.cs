75409eebf82cd2a0311e538553d3ec9daabc968f
lsilvpin <lsilvpin@everis.com>
2020-11-02 20:30:45 -0300

    [HttpListener] Implementação de protocolo Http com .Net framework. (Aplicação responde à chamada: http://localhost:5341/)

---
+++ ByteBank.Terminal/Infraestrutura/WebApplication.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Net;
+using System.Net.Mime;
+using System.Text;
+
+namespace ByteBank.Terminal.Infraestrutura
+{
+    public class WebApplication
+    {
+        private const string contentType = "text/html; charset=utf-8";
+        private const string responseContent = "Hello World";
+
+        private readonly string[] prefixes;
+
+        public WebApplication(string[] prefixes)
+        {
+            if (prefixes == null)
+                throw new ArgumentNullException(nameof(prefixes));
+
+            this.prefixes = prefixes;
+        }
+
+        public void Start()
+        {
+            var httpListener = new HttpListener();
+
+            foreach (var prefixe in prefixes)
+                httpListener.Prefixes.Add(prefixe);
+
+            httpListener.Start();
+
+            var context = httpListener.GetContext();
+
+            var request = context.Request;
+            var response = context.Response;
+
+            response.ContentType = contentType;
+            string responseMessage = responseContent;
+            byte[] responseMessageBytes = Encoding.UTF8.GetBytes(responseMessage);
+            response.StatusCode = 200;
+            response.ContentLength64 = responseMessageBytes.Length;
+
+            response.OutputStream.Write(responseMessageBytes, 0, responseMessageBytes.Length);
+            response.OutputStream.Close();
+
+            httpListener.Stop();
+        }
+    }
+}
--- ByteBank.Terminal/Program.cs
+++ ByteBank.Terminal/Program.cs
@@ -1,15 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using ByteBank.Terminal.Infraestrutura;
 
 namespace ByteBank.Terminal
 {
     class Program
     {
+        private const string prefixe = "http://localhost:5341/";
+
         static void Main(string[] args)
         {
+            var prefixes = new string[] { prefixe };
+            var webApp = new WebApplication(prefixes);
+            webApp.Start();
         }
     }
 }
